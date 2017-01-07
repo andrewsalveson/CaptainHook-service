4939a5d01f4787b980d45a2bca6251a2b3ba7962
Andrew Salveson <andrewsalveson@gmail.com>
2017-01-07 17:50:06 -0500

    add upload script and route using busboy

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+var util    = require('util');
+var spawn   = require('child_process').spawn;
 var express = require('express');
 var app     = express();
 var morgan  = require('morgan');
+var busboy  = require('connect-busboy');
+var fs      = require('fs');
 var port    = 8080;
 
 app.use(morgan('dev')); // log every request to the console
 
-// grasshopper handler
+app.use(busboy());
+
 app.post('/file/gh',function(req,res){
   return res.json({message:"grasshopper file submitted"+arg});
 });
@@ -13,8 +18,51 @@ app.post('/file/dyn',function(req,res){
   return res.json({message:"dynamo file submitted"})
 });
 app.post('/file/osm',function(req,res){
-  exec('./py/osmDiff.py');
-  return res.json({message:"openStudio measures file submitted"});
+  req.pipe(req.busboy);
+  var complete = 0;
+  var oldStream, newStream;
+  var tmpName = String(Math.random()).substr(10)+String((new Date()).getTime());
+  var tmpPath = __dirname + '/tmp/'+tmpName;
+  var oldPath = tmpPath+'old';
+  var newPath = tmpPath+'new';
+  var onComplete = function(){
+    if(complete==2){
+      console.log('all files uploaded');
+      var diff = spawn('ruby',['./rb/osm_diff.rb',oldPath,newPath]);
+      diff.stdout.on('data',function(data){
+        res.write(data);
+      });
+      diff.stderr.on('data',function(data){
+        console.log('error:',data);
+        res.write(data);
+      });
+      diff.on('exit',function(code){
+        res.end();
+        fs.unlink(oldPath);
+        fs.unlink(newPath);
+      });
+    }
+  };
+  req.busboy.on('file',function(fieldname,file,filename){
+    if(fieldname=="model"){
+      oldStream = fs.createWriteStream(oldPath);
+      file.pipe(oldStream);
+      oldStream.on('close',function(){
+        console.log('model uploaded');
+        complete++;
+        onComplete();
+      });
+    }
+    if(fieldname=="compare"){
+      newStream = fs.createWriteStream(newPath);
+      file.pipe(newStream);
+      newStream.on('close',function(){
+        console.log('compare uploaded');
+        complete++;
+        onComplete();
+      });
+    }
+  });
 });
 app.post('/viz',function(req,res){
   return res.send("what am I doing?");
