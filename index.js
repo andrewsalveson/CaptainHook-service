99cc6fd0e99a0ab85c3895070c9c3df85718cb4b
Andrew Salveson <andrewsalveson@gmail.com>
2017-01-07 20:08:52 -0500

    move command around

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.post('/file/osm',function(req,res){
       console.log('oldPath',oldPath);
       console.log('newPath',newPath);
       // var diff = spawn('ruby',['']);
-      var diff = spawn('ruby',['/var/www/rb/osm_diff.rb',oldPath,newPath]);
+      var diff = spawn('ruby /var/www/rb/osm_diff.rb',['',oldPath,newPath]);
       diff.stdout.on('data',function(sdata){
         res.write(sdata);
       });
