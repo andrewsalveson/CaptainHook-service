5e1c97771382023c2a62c3d6ff954335b4f5babe
Andrew Salveson <andrewsalveson@gmail.com>
2017-01-07 20:46:46 -0500

    so long neverneverland

--- rb/osm_diff.rb
+++ rb/osm_diff.rb
@@ -2,13 +2,12 @@
 model_path = ARGV[0]
 compare_model_path = ARGV[1]
 
-require 'neverneverland'
+# require 'neverneverland'
 # require 'csv'
 
     # puts "model_path: #{model_path}"
     # puts "compare_model_path: #{compare_model_path}"
 
-    
 require 'openstudio'
 
 #example
